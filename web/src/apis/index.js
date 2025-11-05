b48a8b28ae8551b3e5150404bdf899caf0b80fb2
nhphung <nhphuung@gmail.com>
2025-11-05 10:39:03 +0700

    feat: implement historyController

--- web/src/apis/index.js
+++ web/src/apis/index.js
@@ -10,3 +10,8 @@ export const fetchDashboardDataAPI = async(userId) => {
   const response = await authorizedAxiosInstance.get(`${API_ROOT}/v1/dashboard/${userId}`)
   return response.data
 }
+
+export const fetchHistoryDataAPI = async(userId, numPage, limit, search, settled) => {
+  const response = await authorizedAxiosInstance.get(`${API_ROOT}/v1/history/${userId}?page=${numPage}&limit=${limit}&search=${search}&settled=${settled}`)
+  return response.data
+}
