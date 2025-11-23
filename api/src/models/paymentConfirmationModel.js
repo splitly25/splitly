5de56ac1e53bf61bf4acbedbe1a56a028947c4f2
Gia Thành <71538542+xing215@users.noreply.github.com>
2025-11-23 11:37:05 +0700

    fix: optional priorityBill

--- api/src/models/paymentConfirmationModel.js
+++ api/src/models/paymentConfirmationModel.js
@@ -17,7 +17,7 @@ const PAYMENT_CONFIRMATION_SCHEMA = Joi.object({
   amount: Joi.number().required(),
   isConfirmed: Joi.boolean().required(), // true = confirmed, false = rejected
   confirmedAt: Joi.date().timestamp('javascript').default(Date.now),
-  priorityBill: Joi.string().optional(),
+  priorityBill: Joi.string().optional().allow(null),
   _destroy: Joi.boolean().default(false)
 })
 
